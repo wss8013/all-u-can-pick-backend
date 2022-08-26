50d9541c7fb7f62626986a882d43dc35cb12735a
wss8013 <wss8013@gmail.com>
2022-08-25 23:02:12 -0700

    add dao and necessary git file

--- dao/farmDAO.js
+++ dao/farmDAO.js
@@ -14,20 +14,17 @@ export default class FarmsDao {
         }
     }
 
+    
     static async getFarms({
         filters = null,
         page = 0,
         farmsPerPage = 20,
-    } = {}) {// empty object is default parameter is case arg is undefined 
+    } = {}) {
         let query;
         if (filters) {
             if ("farm_name" in filters) {
                 query = { $text: { $search: filters['farm_name'] } }
             } 
-            // add fruit filter 
-            // else if ("rated" in filters) {
-            //     query = { "rated": { $eq: filters['rated'] } }
-            // }
         }
         let cursor;
         try {
@@ -42,19 +39,6 @@ export default class FarmsDao {
             return { farmsList: [], totalNumFarms: 0 };
         }
     }
-
-    // todo: implement fruit filter when db is ready 
-    // static async getRatings() {
-    //     let ratings = [];
-    //     try {
-
-    //         ratings = await movies.distinct("rated");
-    //         return ratings;
-    //     } catch (e) {
-    //         console.error(`Unable to get ratings, ${e}`);
-    //         return ratings;
-    //     }
-    // }
 
     static async getFarmById(id) {
         try {
@@ -79,30 +63,27 @@ export default class FarmsDao {
             throw e;
         }
     }
-// todo: may implement when needed 
 
-//     static async getMoviesByIds(ids) {
-//         let cursor;
-//         try {
-//             if (!ids) {
-//                 return;
-//             }
-//             let objectIds = [];
-//             for (let i = 0; i < ids.length; i++) {
-//                 objectIds.push(new ObjectId(ids[i]));
-//             }
-
-//             console.error(objectIds);
-//             cursor = await movies.find({
-//                 _id: { $in: objectIds }
-//             });
-//             console.error(ids);
-//             const favorites = await cursor.toArray();
-//             console.error(favorites);
-//             return favorites;
-//         } catch (e) {
-//             console.error(`Something went wrong in getMoviesByIds: ${e}`);
-//             throw e;
-//         }
-//     }
+    static async getFarmsByIds(ids) {
+        let cursor;
+        try {
+            if (!ids) {
+                return;
+            }
+            let objectIds = [];
+            for (let i = 0; i < ids.length; i++) {
+                objectIds.push(new ObjectId(ids[i]));
+            }
+            cursor = await farms.find({
+                _id: { $in: objectIds }
+            });
+            console.error(ids);
+            const favorites = await cursor.toArray();
+            console.error(favorites);
+            return favorites;
+        } catch (e) {
+            console.error(`Something went wrong in getFarmsByIds: ${e}`);
+            throw e;
+        }
+    }
 }
